db11ab34f85f7b2f6c54bc776f5c329d71a09037
greathero0 <greathero0@126.com>
2022-07-26 15:14:01 +0800

    Add Dot Cross + - for MyVector3

--- Assets/Scripts/MyVector3.cs
+++ Assets/Scripts/MyVector3.cs
@@ -6,4 +6,26 @@ public struct MyVector3
     {
         this.x = x; this.y = y; this.z = z;
     }
+
+    public static float Dot(MyVector3 lhs, MyVector3 rhs)
+    {
+        return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
+    }
+
+    public static MyVector3 Cross(MyVector3 lhs, MyVector3 rhs)
+    {
+        return new MyVector3(lhs.y * rhs.z - lhs.z * rhs.y,
+                             lhs.z * rhs.x - lhs.x * rhs.z,
+                             lhs.x * rhs.y - lhs.y * rhs.x);
+    }
+
+    public static MyVector3 operator +(MyVector3 a, MyVector3 b)
+    {
+        return new MyVector3(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector3 operator -(MyVector3 a, MyVector3 b)
+    {
+        return new MyVector3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
 }
