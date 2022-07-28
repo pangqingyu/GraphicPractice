0b731557384c0252692018efe8d4f1d3d4841de3
greathero0 <greathero0@126.com>
2022-07-28 14:41:50 +0800

    Draw wireframe and fix bugs

--- Assets/Scripts/MyCamera.cs
+++ Assets/Scripts/MyCamera.cs
@@ -42,11 +42,17 @@ public class MyCamera : MonoBehaviour
             && 0 < v.z && v.z < v.w;
     }
 
+    bool CheckLine(Vertex vertex1, Vertex vertex2)
+    {
+        return CheckVertex(vertex1)
+            || CheckVertex(vertex2);
+    }
+
     bool CheckTriangle(MyTriangle triangle)
     {
         return CheckVertex(triangle.p1)
-            && CheckVertex(triangle.p2)
-            && CheckVertex(triangle.p3);
+            || CheckVertex(triangle.p2)
+            || CheckVertex(triangle.p3);
     }
 
     void DrawMesh()
@@ -57,15 +63,15 @@ public class MyCamera : MonoBehaviour
             {
                 p1 = new Vertex
                 {
-                    posInObjectSpace = new Appdata(myMesh.vertices[i], myMesh.uvs[i])
+                    posInObjectSpace = new Appdata(myMesh.vertices[myMesh.triangles[i]], myMesh.uvs[myMesh.triangles[i]])
                 },
                 p2 = new Vertex
                 {
-                    posInObjectSpace = new Appdata(myMesh.vertices[i + 1], myMesh.uvs[i + 1])
+                    posInObjectSpace = new Appdata(myMesh.vertices[myMesh.triangles[i + 1]], myMesh.uvs[myMesh.triangles[i + 1]])
                 },
                 p3 = new Vertex
                 {
-                    posInObjectSpace = new Appdata(myMesh.vertices[i + 2], myMesh.uvs[i + 2])
+                    posInObjectSpace = new Appdata(myMesh.vertices[myMesh.triangles[i + 2]], myMesh.uvs[myMesh.triangles[i + 2]])
                 }
             };
             DrawTriangle(myTriangle);
@@ -81,12 +87,8 @@ public class MyCamera : MonoBehaviour
         return vertex;
     }
 
-    void DrawPoint()
+    void DrawPoint(Vertex p)
     {
-        Vertex p = new Vertex
-        {
-            posInObjectSpace = new Appdata(MyVector3.zero, new MyVector2())
-        };
         p.posInClipSpace = Vert(p.posInObjectSpace);
         if (CheckVertex(p))
         {
@@ -97,9 +99,64 @@ public class MyCamera : MonoBehaviour
             {
                 for (int j = 0; j < ScreenHeight; j++)
                 {
-                    if (Mathf.Abs(i - x) + Mathf.Abs(j - y) < 10)
-                    target.SetPixel(i, j, Color.white);
+                    if (Mathf.Abs(i - x) + Mathf.Abs(j - y) < 1)
+                        target.SetPixel(i, j, Color.white);
                 }
+            }
+        }
+    }
+
+    void DrawLine(Vertex vertex1, Vertex vertex2)
+    {
+        if (!CheckLine(vertex1, vertex2))
+            return;
+
+        int startXIndex = Mathf.RoundToInt(vertex1.posInScreenSpace.x);
+        int endXIndex = Mathf.RoundToInt(vertex2.posInScreenSpace.x);
+        int startYIndex = Mathf.RoundToInt(vertex1.posInScreenSpace.y);
+        int endYIndex = Mathf.RoundToInt(vertex2.posInScreenSpace.y);
+
+        float y21 = vertex2.posInScreenSpace.y - vertex1.posInScreenSpace.y;
+        float x21 = vertex2.posInScreenSpace.x - vertex1.posInScreenSpace.x;
+        float c = vertex1.posInScreenSpace.x * vertex2.posInScreenSpace.y
+                - vertex2.posInScreenSpace.x * vertex1.posInScreenSpace.y;
+
+        // false means -1 <= k <= 1
+        bool flag = Mathf.Abs(y21) > Mathf.Abs(x21);
+        // same point
+        if (startXIndex == endXIndex && startYIndex == endYIndex)
+        {
+            DrawPoint(vertex1);
+        }
+        // vertical line
+        else if (flag)
+        {
+            if (startYIndex > endYIndex)
+            {
+                // swap start end index
+                startYIndex = endYIndex + 0 * (endYIndex = startYIndex);
+            }
+            for (int i = Mathf.Max(startYIndex, 0); i <= Mathf.Min(endYIndex, ScreenHeight - 1); i++)
+            {
+                float x = (x21 * i + c) / y21;
+                int xInt = Mathf.RoundToInt(x);
+                if (xInt >= 0 && xInt < ScreenWidth)
+                    target.SetPixel(xInt, i, Color.white);
+            }
+        }
+        else
+        {
+            if (startXIndex > endXIndex)
+            {
+                // swap start end index
+                startXIndex = endXIndex + 0 * (endXIndex = startXIndex);
+            }
+            for (int i = Mathf.Max(startXIndex, 0); i <= Mathf.Min(endXIndex, ScreenWidth - 1); i++)
+            {
+                float y = (y21 * i - c) / x21;
+                int yInt = Mathf.RoundToInt(y);
+                if (yInt >= 0 && yInt < ScreenHeight)
+                    target.SetPixel(i, yInt, Color.white);
             }
         }
     }
@@ -108,7 +165,15 @@ public class MyCamera : MonoBehaviour
     {
         triangle.p1.posInClipSpace = Vert(triangle.p1.posInObjectSpace);
         triangle.p2.posInClipSpace = Vert(triangle.p2.posInObjectSpace);
-        triangle.p2.posInClipSpace = Vert(triangle.p3.posInObjectSpace);
+        triangle.p3.posInClipSpace = Vert(triangle.p3.posInObjectSpace);
+
+        triangle.p1.posInScreenSpace = CalPosInScreenSpace(triangle.p1.posInClipSpace.vertex);
+        triangle.p2.posInScreenSpace = CalPosInScreenSpace(triangle.p2.posInClipSpace.vertex);
+        triangle.p3.posInScreenSpace = CalPosInScreenSpace(triangle.p3.posInClipSpace.vertex);
+
+        DrawLine(triangle.p1, triangle.p2);
+        DrawLine(triangle.p2, triangle.p3);
+        DrawLine(triangle.p3, triangle.p1);
     }
 
     V2f Vert(Appdata i)
@@ -123,8 +188,11 @@ public class MyCamera : MonoBehaviour
     {
         mvp = GetProjectionMatrix() * GetViewMatrix() * MyMatrix4x4.FromTransform(myMesh.transform);
         ClearScreen();
-        //DrawMesh();
-        DrawPoint();
+        DrawMesh();
+        //DrawPoint(new Vertex
+        //{
+        //    posInObjectSpace = new Appdata(MyVector3.zero, new MyVector2())
+        //});
         target.Apply();
     }
 
